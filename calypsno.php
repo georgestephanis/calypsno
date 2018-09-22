7381fc465ef2cf9df888e772c68b5d20200f8105
George Stephanis <George@Stephanis.info>
2018-09-22 13:44:05 -0400

    Update calypsno.php

--- calypsno.php
+++ calypsno.php
@@ -2,7 +2,7 @@
 
 /*
  * Plugin Name: Calypsno
- * Plugin Description: Disable front-end links to the WordPress.com "Calypso" admin panel.
+ * Description: Disable front-end links to the WordPress.com "Calypso" admin panel.
  */
 
 add_filter( 'pre_option_jetpack_edit_links_calypso_redirect', '__return_null' );
